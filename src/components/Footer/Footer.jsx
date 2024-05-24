16b43916e421bf71ed1b19f8a0fc5654e35aa85e
lucarl07 <junitinho2007@gmail.com>
2024-05-24 12:08:49 -0300

    Finish the basic style for the Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,17 +2,37 @@
 import { Link } from "react-router-dom";
 
 // Assets and components:
-import TransparentLogo from '../../assets/images/logo-transparent.png'
-import { FooterCont } from "./FooterStyle";
+// import TransparentLogo from '../../assets/images/logo-transparent.png'
+import { FooterCont, MiddleBar, LowerBar, FooterNav, SectionLine } from "./FooterStyle";
+import { NavItem } from "../Header/HeaderStyle"
 
 const Footer = () => {
   return (
     <FooterCont>
-      <div className="copyright">
-        <p>&copy; 2024 lucarl07</p>
-        <p>Alguns direitos reservados.</p>
-      </div>
-      
+      <FooterNav>
+        <NavItem>
+          <Link to="/" className="routerLink">Início</Link>
+        </NavItem>
+        <NavItem>
+          <Link to="/sobre" className="routerLink">Sobre</Link>
+        </NavItem> 
+        <NavItem>
+          <Link to="/discografia" className="routerLink">Discografia</Link>
+        </NavItem> 
+        <NavItem>
+          <Link to="/contato" className="routerLink">Contato</Link>
+        </NavItem> 
+      </FooterNav>
+      <SectionLine/>
+      <MiddleBar>
+        <div className="copyright">
+          <p>&copy; 2024 lucarl07</p>
+          <p>Alguns direitos reservados.</p>
+        </div>
+      </MiddleBar>
+      <LowerBar>
+        <p>Digite aqui alguma rede social...</p>
+      </LowerBar>
     </FooterCont>
   );
 }
--- src/components/Footer/FooterStyle.js
+++ src/components/Footer/FooterStyle.js
@@ -1,8 +1,38 @@
 import styled from "styled-components";
 
 export const FooterCont = styled.footer `
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   width: auto;
-  height: 10rem;
-  background-color: var(--black);
+  height: 14rem;
   box-shadow: 0px 0px 16px var(--very-dark-grey);
+  font-family: "Platypi", serif;
 `;
+
+export const FooterNav = styled.nav `
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding: 0 5rem;
+  height: 4rem;
+  background-color: var(--bright-red);
+  list-style-type: none;
+  gap: 3rem;
+`;
+
+export const MiddleBar = styled.section `
+  padding: 1rem;
+  height: 6rem;
+  background-color: var(--very-dark-grey);
+`;
+
+export const LowerBar = styled.section `
+  padding: 1rem;
+  height: 4rem;
+  background-color: var(--white);
+`;
+
+export const SectionLine = styled.hr `
+  color: var(--white);
+`
